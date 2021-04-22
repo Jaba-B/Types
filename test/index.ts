271d57031f97bdd0daf5b0d9e8e98c24d26264a2
Vadym Korotkyi <vadym_korotkyi@epam.com>
2021-04-22 21:34:01 +0000

    Update test cases

--- test/index.ts
+++ test/index.ts
@@ -7,20 +7,20 @@ describe('getDaysToNewYear', () => {
         assert.strictEqual(getDaysToNewYear('31.12.2021'), 1);
     });
 
-    it('should return 72 for given input "22.10.2021"', () => {
-        assert.strictEqual(getDaysToNewYear('22.10.2021'), 72);
+    it('should return 71 for given input "22.10.2021"', () => {
+        assert.strictEqual(getDaysToNewYear('22.10.2021'), 71);
     });
 
-    it('should return 609 for given input "03.05.2020"', () => {
-        assert.strictEqual(getDaysToNewYear('03.05.2020'), 609);
+    it('should return 608 for given input "03.05.2020"', () => {
+        assert.strictEqual(getDaysToNewYear('03.05.2020'), 608);
     });
 
     it('should return 1 for given input new Date("12-31-2021")', () => {
         assert.strictEqual(getDaysToNewYear(new Date('12-31-2021')), 1);
     });
 
-    it('should return 201 for given input new Date(2021, 5, 15)', () => {
-        assert.strictEqual(getDaysToNewYear(new Date(2021, 5, 15)), 201);
+    it('should return 200 for given input new Date(2021, 5, 15)', () => {
+        assert.strictEqual(getDaysToNewYear(new Date(2021, 5, 15)), 200);
     });
 
     it('should return 3586 for given input new Date(2012, 2, 8)', () => {
