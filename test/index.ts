f95c675f1696e0e2ffd98f9d79060f4fd8f001ff
Vadym Korotkyi <vadym_korotkyi@epam.com>
2021-04-22 20:53:47 +0000

    Update test cases

--- test/index.ts
+++ test/index.ts
@@ -3,28 +3,28 @@ import {getDaysToNewYear, lastToFirst, groupOrgUsers} from '../src';
 
 describe('getDaysToNewYear', () => {
 
-    it('should return 1 for given input "31.12.2020"', () => {
-        assert.strictEqual(getDaysToNewYear('31.12.2020'), 1);
+    it('should return 1 for given input "31.12.2021"', () => {
+        assert.strictEqual(getDaysToNewYear('31.12.2021'), 1);
     });
 
-    it('should return 71 for given input "22.10.2020"', () => {
-        assert.strictEqual(getDaysToNewYear('22.10.2020'), 71);
+    it('should return 72 for given input "22.10.2021"', () => {
+        assert.strictEqual(getDaysToNewYear('22.10.2021'), 72);
     });
 
-    it('should return 609 for given input "03.05.2019"', () => {
-        assert.strictEqual(getDaysToNewYear('03.05.2019'), 609);
+    it('should return 609 for given input "03.05.2020"', () => {
+        assert.strictEqual(getDaysToNewYear('03.05.2020'), 609);
     });
 
-    it('should return 1 for given input new Date("12-31-2020")', () => {
-        assert.strictEqual(getDaysToNewYear(new Date('12-31-2020')), 1);
+    it('should return 1 for given input new Date("12-31-2021")', () => {
+        assert.strictEqual(getDaysToNewYear(new Date('12-31-2021')), 1);
     });
 
-    it('should return 200 for given input new Date(2020, 5, 15)', () => {
-        assert.strictEqual(getDaysToNewYear(new Date(2020, 5, 15)), 200);
+    it('should return 201 for given input new Date(2021, 5, 15)', () => {
+        assert.strictEqual(getDaysToNewYear(new Date(2021, 5, 15)), 201);
     });
 
-    it('should return 3221 for given input new Date(2012, 2, 8)', () => {
-        assert.strictEqual(getDaysToNewYear(new Date(2012, 2, 8)), 3221);
+    it('should return 3586 for given input new Date(2012, 2, 8)', () => {
+        assert.strictEqual(getDaysToNewYear(new Date(2012, 2, 8)), 3586);
     });
 
 });
